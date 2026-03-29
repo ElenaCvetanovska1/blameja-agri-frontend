1ad4a3c41b0a7d82284ddd45b8ebc3e1b7bac861
Elena Cvetanovska <elena.cvetanovska@endava.com>
2026-03-29 20:17:47 +0200

    fix fiscal page

--- frontend/app/(app)/(fiscal)/page.tsx
+++ frontend/app/(app)/(fiscal)/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { useFiscalReports } from '~/(app)/(fiscal-receipts)/hooks/useFiscalReports';
-import { useCashOperation } from '~/(app)/(fiscal-receipts)/hooks/useCashOperation';
+import { useFiscalReports } from '../(fiscal-receipts)/hooks/useFiscalReports';
+import { useCashOperation } from '../(fiscal-receipts)/hooks/useCashOperation';
 import { useDatetimeSync } from './hooks/useDatetimeSync';
 import { useLastEntry, LAST_ENTRY_TYPES } from './hooks/useLastEntry';
 import { useMemoryReport } from './hooks/useMemoryReport';
